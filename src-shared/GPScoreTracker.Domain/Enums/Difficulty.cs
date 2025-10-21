c91de9ffff561890e81dccd3559e843879e1145f
kanase kazuyoshi <k.kanase1547@outlook.jp>
2025-10-21 14:48:58 +0900

    不要なコメントを削除

--- src-shared/GPScoreTracker.Domain/Enums/Difficulty.cs
+++ src-shared/GPScoreTracker.Domain/Enums/Difficulty.cs
@@ -5,28 +5,13 @@ namespace GPScoreTracker.Domain.Enums;
 /// </summary>
 public enum Difficulty
 {
-    /// <summary>
-    /// 初心者向け
-    /// </summary>
     Beginner = 1,
 
-    /// <summary>
-    /// 基本
-    /// </summary>
     Basic = 2,
 
-    /// <summary>
-    /// 難関
-    /// </summary>
     Difficult = 3,
 
-  /// <summary>
-    /// 達人
-    /// </summary>
     Expert = 4,
 
-    /// <summary>
-    /// 挑戦
-    /// </summary>
     Challenge = 5
 }
